070d1114d4b4489a4145e3aaffff9b5ffdc61f96
Alan Jean <afinegameofnil@gmail.com>
2023-02-25 21:59:47 +0400

    chore: initialize project environment

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const Koa = require("koa");
+const logger = require("koa-logger");
+const cors = require("@koa/cors");
+const bodyParser = require("koa-bodyparser");
+const _ = require("koa-route");
+
+const app = new Koa();
+
+const port = process.env.PORT || 3000;
+
+// prettier-ignore
+app
+    .use(logger())
+    .use(cors())
+    .use(bodyParser())
+    .listen(port);
+
+console.log(`Listening on port ${port}`);
