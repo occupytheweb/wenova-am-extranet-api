396930a9002faaa8fd1d87157a67334ff8d1e42b
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 12:15:02 +0400

    chore: switch out 'koa-route' for '@koa/router'

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const Koa = require("koa");
 const logger = require("koa-logger");
 const cors = require("@koa/cors");
 const bodyParser = require("koa-bodyparser");
-const _ = require("koa-route");
 
 const app = new Koa();
 
