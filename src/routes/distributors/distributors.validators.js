3310d40e8d61843f546f9584a1a837a2f5ad6ac1
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 12:10:02 +0400

    feat(distributors): add distributor-related payload validation

---
+++ src/routes/distributors/distributors.validators.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+
+const getValidatedIdFromRequestParamsIfPossible = (ctx) => {
+  const potentialId = ctx.params.id;
+
+  Joi.assert(potentialId, Joi.number().required(), "'id' should be a number");
+
+  return +potentialId;
+};
+
+const distributorOverviewSchema = Joi.object({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().required(),
+  address: Joi.string().required(),
+});
+
+const distributorBillingSchema = Joi.object({
+  billingEmail: Joi.string().required(),
+  iban: Joi.string().required(),
+});
+
+const getValidatedPartialDistributorPayloadIfPossible = (ctx) => {
+  const potentialDistributor = ctx.request.body;
+
+  Joi.assert(
+    potentialDistributor,
+    Joi.alternatives(distributorOverviewSchema, distributorBillingSchema)
+  );
+
+  return potentialDistributor;
+};
+
+module.exports = {
+  getValidatedIdFromRequestParamsIfPossible,
+  getValidatedPartialDistributorPayloadIfPossible,
+};
