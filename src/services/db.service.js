fd7899d29f55e3ee98327d25545b21c976082bbc
Alan Jean <afinegameofnil@gmail.com>
2023-02-27 03:18:03 +0400

    feat(dal): add initial db provider service

---
+++ src/services/db.service.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql2/promise");
+
+const { dbConnectionProperties } = require("../config");
+
+let pool;
+
+const init = () => {
+  if (!pool) {
+    console.info("[DB] Initializing connection pool...");
+    pool = mysql.createPool(dbConnectionProperties);
+
+    console.info("[DB] Testing connection...");
+    pool
+      .query("SELECT 1 as healthcheck")
+      .then((_) => {
+        console.info("[DB] Connection pool initialized.");
+      })
+      .catch((error) => {
+        console.error("[DB] Failed to connect to db.");
+
+        throw error;
+      });
+  } else {
+    console.error("Connection pool already initialized");
+  }
+};
+
+const getPool = () => pool;
+
+module.exports = {
+  init,
+  getPool,
+};
