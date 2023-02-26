e0df885bb84da469063d57aa6c719b17c214a573
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 18:17:13 +0400

    feat(payments): add the initial payments resource

---
+++ src/resources/payments.resource.js
@@ -0,0 +1,32 @@
+const repository = require("../repositories/payments.repository");
+
+const config = require("../config");
+const pagination = require("../utils/pagination");
+
+const searchByDistributor = (
+  distributorId,
+  page = 1,
+  maxPerPage = config.maxItemsPerPage
+) => {
+  return search(distributorId, {}, page, maxPerPage);
+};
+
+const search = (
+  distributorId,
+  criteria,
+  page = 1,
+  maxPerPage = config.maxItemsPerPage
+) => {
+  const data = repository.searchPaged(distributorId, {}, page, maxPerPage);
+  const count = repository.count(distributorId, {});
+
+  return {
+    data,
+    ...pagination.getPaginationMetadata(count, page, maxPerPage),
+  };
+};
+
+module.exports = {
+  searchByDistributor,
+  search,
+};
