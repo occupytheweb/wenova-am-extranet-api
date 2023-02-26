b341e443e7889b6a153f2a6b40622384391849cc
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 23:19:26 +0400

    feat(auth): derive user-id from authentication context

--- src/routes/payments/payments.routes.js
+++ src/routes/payments/payments.routes.js
@@ -4,22 +4,20 @@ const subscriptions = require("../../resources/payments.resource");
 const validators = require("./payments.validators");
 const pagination = require("../../utils/pagination");
 
+const authService = require("../../services/auth.service");
+
 const router = new Router({
   prefix: "/payments",
 });
 
 router.get("/", (ctx) => {
-  const mockDistributorId = 49;
+  const { userId: distributorId } =
+    authService.getUserFromAuthenticatedRequest(ctx);
 
   const criteria = validators.validateAndGetSearchCriteria(ctx);
   const { page, maxPerPage } = pagination.getPaginationParams(ctx);
 
-  ctx.body = subscriptions.search(
-    mockDistributorId,
-    criteria,
-    page,
-    maxPerPage
-  );
+  ctx.body = subscriptions.search(distributorId, criteria, page, maxPerPage);
 });
 
 module.exports = router;
--- src/routes/subscriptions/subscriptions.routes.js
+++ src/routes/subscriptions/subscriptions.routes.js
@@ -4,22 +4,20 @@ const subscriptions = require("../../resources/subscriptions.resource");
 const validators = require("./subscriptions.validators");
 const pagination = require("../../utils/pagination");
 
+const authService = require("../../services/auth.service");
+
 const router = new Router({
   prefix: "/subscriptions",
 });
 
 router.get("/", (ctx) => {
-  const mockDistributorId = 49;
+  const { userId: distributorId } =
+    authService.getUserFromAuthenticatedRequest(ctx);
 
   const criteria = validators.validateAndGetSearchCriteria(ctx);
   const { page, maxPerPage } = pagination.getPaginationParams(ctx);
 
-  ctx.body = subscriptions.search(
-    mockDistributorId,
-    criteria,
-    page,
-    maxPerPage
-  );
+  ctx.body = subscriptions.search(distributorId, criteria, page, maxPerPage);
 });
 
 module.exports = router;
