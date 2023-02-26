00f09e2c37a03e3435fe1521f5782b648eaac15d
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 21:57:21 +0400

    feat(auth): add the authentication payload validators

---
+++ src/routes/auth/auth.validators.js
@@ -0,0 +1,17 @@
+const Joi = require("joi");
+
+const getValidatedCredentialsPayload = (ctx) => {
+  const { email, password } = ctx.request.body;
+
+  Joi.assert(email, Joi.string().email().required(), "'email' is invalid");
+  Joi.assert(password, Joi.string().required(), "'password' is invalid");
+
+  return {
+    email,
+    password,
+  };
+};
+
+module.exports = {
+  getValidatedCredentialsPayload,
+};
