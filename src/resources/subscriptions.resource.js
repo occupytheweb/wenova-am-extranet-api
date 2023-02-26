6e99803264412e4e2a8ab85d1c98b794439c8ab9
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 17:15:06 +0400

    feat(subscriptions): update the resource to adapt to the search by criteria

--- src/resources/subscriptions.resource.js
+++ src/resources/subscriptions.resource.js
@@ -1,10 +1,32 @@
 const repository = require("../repositories/subscriptions.repository");
 
-const search = (product, distributorId) =>
-  repository.search(product, distributorId);
+const config = require("../config");
+const pagination = require("../utils/pagination");
 
-const search = (product, distributorId, page, maxPerPage) =>
-  repository.search(product, distributorId, page, maxPerPage);
+const searchByDistributor = (
+  distributorId,
+  page = 1,
+  maxPerPage = config.maxItemsPerPage
+) => {
+  return search(distributorId, {}, page, maxPerPage);
+};
+
+const search = (
+  distributorId,
+  criteria,
+  page = 1,
+  maxPerPage = config.maxItemsPerPage
+) => {
+  const data = repository.searchPaged(distributorId, {}, page, maxPerPage);
+  const count = repository.count(distributorId, {});
+
+  return {
+    data,
+    ...pagination.getPaginationMetadata(count, page, maxPerPage),
+  };
+};
+
 module.exports = {
+  searchByDistributor,
   search,
 };
