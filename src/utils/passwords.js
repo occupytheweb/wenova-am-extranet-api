8343b3cd830b887022743e3ddc8e7711d6144530
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 15:26:03 +0400

    feat: add passwords utility module

---
+++ src/utils/passwords.js
@@ -0,0 +1,15 @@
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+
+const hashPlaintextPassword = (plaintextPassword) =>
+  bcrypt
+    .genSalt(SALT_ROUNDS)
+    .then((salt) => bcrypt.hash(plaintextPassword, salt));
+const plaintextPasswordMatchesStoredHash = (plaintextPassword, hash) =>
+  bcrypt.compare(plaintextPassword, hash);
+const generateInitialPassword = (firstName, lastName) =>
+  `${firstName}_${lastName}@wenova`;
+
+const generateInitialPasswordHash = (firstName, lastName) =>
+  hashPlaintextPassword(generateInitialPassword(firstName, lastName));
