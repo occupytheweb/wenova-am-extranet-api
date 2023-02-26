700c955fce7ba23dd7eb0943d91e04d80bc282ca
Alan Jean <afinegameofnil@gmail.com>
2023-02-26 06:31:02 +0400

    feat(distributors): add the initial distributors resource

---
+++ src/resources/distributors.resource.js
@@ -0,0 +1,60 @@
+const repository = require("../repositories/distributors.repository");
+
+const list = () => repository.list();
+
+const exists = (id) => repository.exists(id);
+
+const getById = (id) => {
+  return repository.getById(id);
+};
+
+const update = (id, patchToApply) => {
+  const potentialDistributor = getById(id);
+  const effectivePatchPayload =
+    getEffectiveDistributorPatchPayload(patchToApply);
+
+  const sanitizedNewRepresentation = {
+    ...potentialDistributor,
+    ...effectivePatchPayload,
+  };
+
+  return repository.update(id, sanitizedNewRepresentation);
+};
+
+const create = (payload) => {
+  return repository.create(payload);
+};
+
+/**
+ * Translates keys of an object conforming to either the {@link distributorOverviewSchema} or a
+ * {@link distributorBillingSchema} to their db-level counterparts.
+ */
+const getEffectiveDistributorPatchPayload = (distributorPatchPayload) => {
+  delete distributorPatchPayload["id"];
+
+  const translations = {
+    firstName: "first_name",
+    lastName: "last_name",
+    email: "email_signataire",
+    billingEmail: "email_compta",
+    iban: "IBAN",
+  };
+
+  return Object.entries(distributorPatchPayload).reduce(
+    (accumulator, [key, value]) => ({
+      ...accumulator,
+      ...(!!translations[key]
+        ? { [translations[key]]: value }
+        : { [key]: value }),
+    }),
+    {}
+  );
+};
+
+module.exports = {
+  list,
+  exists,
+  getById,
+  update,
+  create,
+};
