2fa5f0979931765d1e54aa74913fb2854527f960
Alan Jean <afinegameofnil@gmail.com>
2023-02-28 00:08:01 +0400

    feat(dal): migrate from the mock payments repository

--- src/repositories/payments.repository.js
+++ src/repositories/payments.repository.js
@@ -1,39 +1,121 @@
-const db = require("./payments.json");
+const SQL = require("sql-template-strings");
 
+const dbService  = require("../services/db.service");
 const pagination = require("../utils/pagination");
 
 
-const list = () => db;
+const db = () => dbService.getPool();
 
-const search = (distributorId, criteria) => {
+
+const list = () => db()
+  .query(SQL`SELECT * FROM com_note`)
+  .then(
+    ([rows]) => rows
+  )
+;
+
+
+const getSearchCriteriaClause = (
+  distributorId,
+  criteria
+) => {
+  const condition = SQL`
+    WHERE id_dist = ${distributorId}
+  `;
+
   const { year } = criteria;
+  if (!!year) {
+    const yearCriteria = `%${year}%`;
 
-  const filters = [
-    (payment) => payment.id_dist === distributorId,
-    ...(!!year ? [(payment) => /`${year}`/.test(payment.periode)] : []),
-  ];
+    condition.append(
+      SQL`
+        AND periode LIKE ${yearCriteria}
+      `
+    );
+  }
 
-  return filters.reduce(
-    (accumulator, filter) => accumulator.filter(filter),
-    list()
-  );
+  return condition;
 };
 
-const count = (distributorId, criteria) => search(distributorId, criteria).length;
+
+const getPagedSearchCriteriaClause = (
+  distributorId,
+  criteria,
+  startIndex,
+  maxPerPage
+) => getSearchCriteriaClause(
+  distributorId,
+  criteria
+).append(
+  SQL`
+    ORDER BY note_date DESC
+    LIMIT ${startIndex}, ${maxPerPage}
+  `
+);
+
+
+const search = (distributorId, criteria) => db()
+  .query(SQL`SELECT * FROM com_note`)
+  .append(
+    getSearchCriteriaClause(
+      distributorId,
+      criteria
+    )
+  )
+  .then(
+    ([rows]) => rows
+  )
+;
+
+
+const getTotalNumberOfRows = (distributorId, criteria) => db()
+  .query(
+    SQL`SELECT count(1) as count FROM com_note`
+      .append(
+        getSearchCriteriaClause(
+          distributorId,
+          criteria
+        )
+      )
+  )
+  .then(
+    ([rows]) => {
+      const { count } = rows.find(() => true);
+
+      return count;
+    }
+  )
+;
+
 
 const searchPaged = (distributorId, criteria, page, maxPerPage) => {
-  const matchingPayments = search(distributorId, criteria);
-
-  const { startIndex, endIndex } = pagination.getPaginationIndices(
+  const { startIndex } = pagination.getPaginationIndices(
     page,
     maxPerPage
   );
 
-  return matchingPayments.slice(startIndex, endIndex);
+  return db()
+    .query(
+      SQL`SELECT * FROM com_note`
+        .append(
+          getPagedSearchCriteriaClause(
+            distributorId,
+            criteria,
+            startIndex,
+            maxPerPage
+          )
+        )
+    )
+    .then(
+      ([rows]) => rows
+    )
+  ;
 };
 
 
 module.exports = {
-  count,
+  list,
+  search,
   searchPaged,
+  getTotalNumberOfRows,
 };
