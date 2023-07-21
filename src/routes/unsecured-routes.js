03b41fb89fe89b0b7103da093f166a51bd5e8ad4
Alan Jean <afinegameofnil@gmail.com>
2023-07-22 00:23:05 +0400

    feat(forgot-password): allow unauthenticated access to forgotten password API resources

--- src/routes/unsecured-routes.js
+++ src/routes/unsecured-routes.js
@@ -1,6 +1,10 @@
+const encodedAtSign = encodeURIComponent("@");
+
 const unsecuredRoutesPatterns = [
   "/auth/token",
   "/users/me/password/forgotten",
+  `/distributors/.+${encodedAtSign}.+`,
+  `/users/.+${encodedAtSign}.+/password/forgotten`,
 ].map(
   (pattern) => new RegExp(pattern)
 );
