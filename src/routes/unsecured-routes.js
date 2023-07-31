02a0784257a7caba27ac925dbc4b634ff50d3dbd
Alan Jean <afinegameofnil@gmail.com>
2023-07-31 11:25:17 +0200

    feat: handle encoded and unencoded chars in unauthenticated routes

--- src/routes/unsecured-routes.js
+++ src/routes/unsecured-routes.js
@@ -1,10 +1,19 @@
 const encodedAtSign = encodeURIComponent("@");
 
+const routesWithAtSign = [
+  `/distributors/.+@.+`,
+  `/users/.+@.+/password/forgotten`,
+].flatMap(
+  (pattern) => [
+    pattern,
+    pattern.replace("@", encodedAtSign),
+  ]
+);
+
 const unsecuredRoutesPatterns = [
   "/auth/token",
   "/users/me/password/forgotten",
-  `/distributors/.+${encodedAtSign}.+`,
-  `/users/.+${encodedAtSign}.+/password/forgotten`,
+  ...routesWithAtSign,
 ].map(
   (pattern) => new RegExp(pattern)
 );
